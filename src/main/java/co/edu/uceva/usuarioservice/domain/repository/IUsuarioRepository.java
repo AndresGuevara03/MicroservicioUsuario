55f8d56908d83736e099e78604384a22d1b690c4
andres <andres.guevara03@uceva.edu.co>
2025-03-28 22:59:04 -0500

    Creacion de UsuarioRestController, banner.txt, import.sql y se agrego el contenido de application.properties corregir: al inniciar la api no carga correctamente el import.sql

--- src/main/java/co/edu/uceva/usuarioservice/domain/repository/IUsuarioRepository.java
+++ src/main/java/co/edu/uceva/usuarioservice/domain/repository/IUsuarioRepository.java
@@ -2,7 +2,6 @@ package co.edu.uceva.usuarioservice.domain.repository;
 
 import co.edu.uceva.usuarioservice.domain.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 
 public interface IUsuarioRepository extends JpaRepository<Usuario, Long> {
 }
