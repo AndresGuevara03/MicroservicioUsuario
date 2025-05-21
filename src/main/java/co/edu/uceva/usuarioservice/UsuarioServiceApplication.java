3ff587ff84bbcc3d64a2b4e68acc62beefe5e5c0
andres <andres.guevara03@uceva.edu.co>
2025-05-21 13:19:04 -0500

    integracion inicial de Eureka y openfeing

--- src/main/java/co/edu/uceva/usuarioservice/UsuarioServiceApplication.java
+++ src/main/java/co/edu/uceva/usuarioservice/UsuarioServiceApplication.java
@@ -2,10 +2,10 @@ package co.edu.uceva.usuarioservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 public class UsuarioServiceApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(UsuarioServiceApplication.class, args);
     }
